bb4154647f35e5a05c1e1d305adaf4d53854dd3a
noleemits <stevenoleemits@gmail.com>
2024-07-24 01:25:18 -0600

    changed working from networx to fixbeen in shortcode for form 2

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -8,9 +8,9 @@ function fixbee_form_step1() {
 add_shortcode('fixbee_form_step1', 'fixbee_form_step1');
 
 // Shortcode for Step 2 form
-function networx_form_step2() {
+function fixbee_form_step2() {
     ob_start();
-    include NETWORX_PLUGIN_DIR . 'templates/form-step2.php';
+    include FIXBEE_PLUGIN_DIR . 'templates/form-step2.php';
     return ob_get_clean();
 }
-add_shortcode('networx_form_step2', 'networx_form_step2');
+add_shortcode('fixbee_form_step2', 'fixbee_form_step2');
