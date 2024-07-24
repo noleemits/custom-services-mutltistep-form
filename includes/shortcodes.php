20af37f2c4b66f71342ba02b36dc103f8e4a1668
noleemits <stevenoleemits@gmail.com>
2024-07-24 01:24:01 -0600

    Added code shortcode for step 2

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -6,3 +6,11 @@ function fixbee_form_step1() {
     return ob_get_clean();
 }
 add_shortcode('fixbee_form_step1', 'fixbee_form_step1');
+
+// Shortcode for Step 2 form
+function networx_form_step2() {
+    ob_start();
+    include NETWORX_PLUGIN_DIR . 'templates/form-step2.php';
+    return ob_get_clean();
+}
+add_shortcode('networx_form_step2', 'networx_form_step2');
