431fa492aae0d254138cd54b7b3ebe3ab91b12a9
noleemits <stevenoleemits@gmail.com>
2024-07-24 01:28:53 -0600

    Added shortcode for step 3

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -14,3 +14,11 @@ function fixbee_form_step2() {
     return ob_get_clean();
 }
 add_shortcode('fixbee_form_step2', 'fixbee_form_step2');
+
+// Shortcode for Step 3 form
+function fixbee_form_step3() {
+    ob_start();
+    include FIXBEE_PLUGIN_DIR . 'templates/form-step3.php';
+    return ob_get_clean();
+}
+add_shortcode('fixbee_form_step3', 'fixbee_form_step3');
